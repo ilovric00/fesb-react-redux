c3ba39489081f76cb1e01bfd3dc503f7afefeea5
ilovric00 <ilovric@profico.hr>
2017-12-09 11:31:11 +0100

    fixing indentation

--- src/store.js
+++ src/store.js
@@ -14,12 +14,12 @@ import { createStore } from 'redux';
  */
 function reducer(state = 0, action) {
   switch (action.type) {
-  case 'INCREMENT':
-    return state + 1
-  case 'DECREMENT':
-    return state - 1
-  default:
-    return state
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
   }
 }
 
